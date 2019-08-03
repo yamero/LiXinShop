69078ac275422e220c2475ca003266135072028b
naruto <naruto@konoha.com>
2019-08-03 22:26:46 +0800

    添加支付组件

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Monolog\Logger;
+use Yansongda\Pay\Pay;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,9 +15,34 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        // 如果不是生产环境就将laravel代码提示组件注入到依赖容器中
         if ($this->app->environment() !== 'production') {
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
         }
+
+        $this->app->singleton('alipay', function () {
+            $config = config('pay.alipay');
+            // 判断当前项目运行环境是否为线上环境
+            if (app()->environment() !== 'production') {
+                $config['mode']         = 'dev';
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+            // 调用 Yansongda\Pay 来创建一个支付宝支付对象
+            return Pay::alipay($config);
+        });
+
+        $this->app->singleton('wechat_pay', function () {
+            $config = config('pay.wechat');
+            if (app()->environment() !== 'production') {
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+            // 调用 Yansongda\Pay 来创建一个微信支付对象
+            return Pay::wechat($config);
+        });
     }
 
     /**
